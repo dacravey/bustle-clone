df46253aa45d18825259b79d94bd7844693ace5f
Debbie Cravey and Jackie Fletcher <cinemaestro@yahoo.com>
2015-03-02 16:28:10 -0800

    add bootstrap carousel - WIP

--- controllers/home.js
+++ controllers/home.js
@@ -10,35 +10,3 @@ HustledBustle.HomeController = Ember.Controller.extend({
     // }
   }
 });
-
-
-
-//Ember version (not yet functioning)
-// HustledBustle.viewArray = Ember.A(["aTemplate", "bTemplate", "cTemplate"]);
-
-// currentIndex: 0,
-//   selectedView: function() {
-//     currentIndex = this.get('currentIndex');
-//     template = HustledBustle.viewArray.get(currentIndex);
-//       return Ember.View.create({
-//           templateName: template,
-//           classNames: ["carousel-element"],
-//           didInsertElement: function() {
-//               $(this.$()[0]).animate({
-//                   opacity: 1,
-//                   marginLeft: '+=100'
-//               }, 500);
-//           }
-//       });
-//   }.property('currentIndex'),
-//   previous: function() {
-//       if (this.get('currentIndex') !== 0) {
-//           currentIndex = this.get('currentIndex');
-//           this.set('currentIndex', currentIndex - 1);
-//       }
-//   },
-//   next: function() {
-//       if (this.get('currentIndex') !== (HustledBustle.viewArray.length - 1)) {
-//           currentIndex = this.get('currentIndex');
-//           this.set('currentIndex', currentIndex + 1);
-//       }
